e31850e61919538aaef80ec90855088024392a3f
yeolife <yeo2507@naver.com>
2023-08-08 20:11:41 +0900

    UI 완성

---
+++ WeatherToday/TableViewCell_country.swift
@@ -0,0 +1,23 @@
+//
+//  TableViewCell_country.swift
+//  WeatherToday
+//
+//  Created by yeolife on 2023/08/08.
+//
+
+import UIKit
+
+class TableViewCell_country: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ WeatherToday/ViewController_info.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController_info.swift
+//  WeatherToday
+//
+//  Created by yeolife on 2023/08/08.
+//
+
+import UIKit
+
+class ViewController_info: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
